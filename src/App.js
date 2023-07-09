e9acfe1bde8feecab2ded659190d22a0e635facd
Afreen063 <afreen0501@gmail.com>
2023-07-09 21:22:02 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,69 +1,80 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
-import { useState } from 'react';
-import './App.css'
-
-
-
+import React from "react";
+import ReactPlayer from "react-player";
+import { useState } from "react";
+import urls from "./Constants/constants"; //Contain an array of object [{url:string,thumbnail:string}]
 
 function App() {
+  const [currentUrl, setCurrentUrl] = useState(
+    "https://www.youtube.com/watch?v=CsOsmgUmT9U"
+  );
+  const [hover, setHover] = useState(false); //to add the hover Effect
 
-  const [currentUrl, setCurrentUrl] = useState('https://www.youtube.com/watch?v=CsOsmgUmT9U')
-  const [ hover,setHover]=useState(false)
-  
-  const urls = [
-    {
-      url: 'https://www.youtube.com/watch?v=M9DblLvu7Sc',
-      thumbnail: 'https://img.youtube.com/vi/M9DblLvu7Sc/maxresdefault.jpg'
-    },
-    {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-    },
-     {
-      url: 'https://www.youtube.com/watch?v=CsOsmgUmT9U',
-      thumbnail:'https://img.youtube.com/vi/CsOsmgUmT9U/maxresdefault.jpg'
-    },
-      {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-    },
-       {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-    },
-        {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-        }
-        , {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-    }, {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-    }, {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-    }, {
-      url: 'https://www.youtube.com/watch?v=bYRdtCGmviM',
-      thumbnail:'https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg'
-    }
-  ]
   return (
-    <div className="App" >
-      
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        width: "57%",
+        margin: "auto",
+        flexDirection: "column",
+        alignItems: "center",
+        border: "4px solid #764646",
+        borderRadius: "20px",
+      }}
+    >
+      <h1 style={{ fontWeight: "bold", color: "#545421" }}>Video Player</h1>
+      {/** Video player from react-players */}
       <ReactPlayer url={currentUrl} />
-   
-      <div className='thumbnail' onMouseOver={()=>{setHover(true)}} onMouseLeave={()=>{setHover(false)}} style={{ width: '650px', display:'flex', flexDirection:'row', overflowX:'scroll'}} >
-       
-        {hover?urls.map((item,index) => {
-          return (
-            
-              <img key={String(index)} alt='thumbnail' src={ item.thumbnail}style={{width:'100px',borderRadius:'50%'}} onClick={()=>{setCurrentUrl(item.url)}} />
-            
-          )
-        }):null}
+
+      <div
+        className="thumbnail"
+        onMouseOver={() => {
+          setHover(true);
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
+        style={{
+          marginTop: "10px",
+          width: "650px",
+          display: "flex",
+          flexDirection: "row",
+          cursor: "pointer",
+          overflowX: hover ? "scroll" : "auto",
+        }}
+      >
+        {hover ? (
+          urls.map((item, index) => {
+            return (
+              <img
+                key={String(index)}
+                alt="thumbnail"
+                src={item.thumbnail}
+                style={{
+                  width: "100px",
+                  height: "100px",
+                  borderRadius: "50%",
+                  margin: "5px",
+                }}
+                onClick={() => {
+                  setCurrentUrl(item.url);
+                }}
+              />
+            );
+          })
+        ) : (
+          <div
+            onMouseOver={() => {
+              setHover(true);
+            }}
+            onMouseLeave={() => {
+              setHover(false);
+            }}
+            style={{ color: "#FFFF" }}
+          >
+            {".."}
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/Constants/constants.js
@@ -0,0 +1,68 @@
+const urls = [
+  {
+    url: "https://www.youtube.com/watch?v=M9DblLvu7Sc",
+    thumbnail: "https://img.youtube.com/vi/M9DblLvu7Sc/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=CsOsmgUmT9U",
+    thumbnail: "https://img.youtube.com/vi/CsOsmgUmT9U/maxresdefault.jpg",
+  },
+
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=bYRdtCGmviM",
+    thumbnail: "https://img.youtube.com/vi/bYRdtCGmviM/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=gULskrUm_eg",
+    thumbnail: "https://img.youtube.com/vi/gULskrUm_eg/maxresdefault.jpg",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=69IbzTWg5PM",
+    thumbnail: "https://img.youtube.com/vi/69IbzTWg5PM/maxresdefault.jpg",
+  },
+];
+export default urls;
